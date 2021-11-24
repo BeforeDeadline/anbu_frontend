f074654497b61e88dca8abe6a996330830fb8c8d
yoon-junseo <ysj6844@naver.com>
2021-11-25 02:12:37 +0900

    Feature: NavigationTitle 생성
    1. 컴포넌트
    2. 스타일
    #3

---
+++ navigaton/NavigationTitle/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { View } from "react-native";
+import LogoIcon from "../../components/atomic/atoms/Icons/LogoIcon";
+import AlarmIcon from "../../components/atomic/atoms/Icons/AlarmIcon";
+import TagIcon from "../../components/atomic/atoms/Icons/TagIcon";
+import PersonPlusIcon from "../../components/atomic/atoms/Icons/PersonPlusIcon";
+
+import { Header, Contents, TitleText, RowBox } from "./style";
+import ArrowIcon from "../../components/atomic/atoms/Icons/ArrowIcon";
+
+const titleList = {
+  anbu: "안부",
+  tag: "태그",
+};
+
+const NavigationTitle = ({ title, props }) => {
+  console.log("nav: ", title, props);
+  return (
+    <Header>
+      <Contents style={{ color: "white" }}>
+        {title === "home" && <LogoIcon />}
+        {title === "navFriend" && <TitleText>친구</TitleText>}
+        {title === ("anbu" || "tag") && (
+          <RowBox width="70px">
+            <ArrowIcon />
+            <TitleText>{titleList[title]}</TitleText>
+          </RowBox>
+        )}
+      </Contents>
+      <View>
+        {title === "home" && <AlarmIcon />}
+        {title === "navFriend" && (
+          <RowBox width="50px">
+            <TagIcon />
+            <PersonPlusIcon />
+          </RowBox>
+        )}
+      </View>
+    </Header>
+  );
+};
+
+export default NavigationTitle;
