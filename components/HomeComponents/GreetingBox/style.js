4aede715acb0c82fae2ad1a59961019078ac4c51
yoon-junseo <ysj6844@naver.com>
2021-11-25 17:35:48 +0900

    Feature: GreetingBox 생성
    1. 컴포넌트
    2. 스타일
    3. 인삿말 box
    4. name으로 사용자명 변경
    #5

---
+++ components/HomeComponents/GreetingBox/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components/native";
+
+import useGetClientWidth from "../../../hooks/useGetClientWidth";
+
+const CLIENT_WIDTH = useGetClientWidth() - 40;
+
+export const Container = styled.View`
+  flex: 0.5;
+  width: ${CLIENT_WIDTH};
+  height: 50px;
+`;
+
+export const LargeText = styled.Text`
+  font-size: 24px;
+  font-weight: 400;
+  color: #ffffff;
+`;
+
+export const LargeBoldText = styled.Text`
+  font-size: 26px;
+  font-weight: 600;
+  color: #ffffff;
+`;
