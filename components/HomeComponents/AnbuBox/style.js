fb8cedb751534133f47ff8a9aa19bbfb43e8b6b0
yoon-junseo <ysj6844@naver.com>
2021-11-25 17:33:48 +0900

    Feature: AnbuBox 생성
    1. 컴포넌트
    2. 스타일
    3. 안부를 물을 친구 목록
    4. 생일인 친구 목록
    3. type으로 구분
    #5

---
+++ components/HomeComponents/AnbuBox/style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+
+import useGetClientWidth from "../../../hooks/useGetClientWidth";
+
+const CLIENT_WIDTH = useGetClientWidth() - 40;
+
+export const TitleBox = styled.View`
+  flex-direction: row;
+
+  margin-top: 20px;
+
+  width: ${CLIENT_WIDTH};
+`;
+
+export const TitleContents = styled.Text`
+  margin-left: ${(props) => props.marginLeft};
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => props.fontColor};
+`;
+
+export const ScrollBox = styled.ScrollView`
+  flex: 1;
+`;
