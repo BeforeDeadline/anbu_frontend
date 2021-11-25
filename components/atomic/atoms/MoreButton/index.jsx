1cb3c97f71210adfef4b19e0d2f783af3009a5ca
yoon-junseo <ysj6844@naver.com>
2021-11-26 00:13:29 +0900

    Feature: MoreButton 생성
    1. MoreButton 컴포넌트 생성
    + 추후에 해당 컴포넌트로 다른 버튼들 교체 
    #9

---
+++ components/atomic/atoms/MoreButton/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styled from "styled-components/native";
+
+const Container = styled.Text`
+  font-size: 10px;
+  color: #e1e1e1;
+`;
+
+const MoreButton = () => {
+  return <Container>ㆍㆍㆍ</Container>;
+};
+
+export default MoreButton;
