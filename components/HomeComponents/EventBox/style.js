ec2308bc491d5721a1ad14c0194103ad40d9a5bb
yoon-junseo <ysj6844@naver.com>
2021-11-25 17:35:07 +0900

    Feature: EventBox 생성
    1. 컴포넌트
    2. 스타일
    3. 이벤트 관련 box (현재는 props로 받는게 아니라 직접 입력 -> 추후 확장하면 리스트 항목을 컴포넌트화)
    #5

---
+++ components/HomeComponents/EventBox/style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components/native";
+
+import useGetClientWidth from "../../../hooks/useGetClientWidth";
+
+const CLIENT_WIDTH = useGetClientWidth() - 40;
+const EVENT_INNER_WIDTH = CLIENT_WIDTH - 36;
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+export const EventItemContainer = styled.View`
+  justify-content: center;
+  width: ${CLIENT_WIDTH};
+  height: 56px;
+
+  margin-top: 8px;
+  padding-left: 10px;
+
+  background-color: ${(props) => props.backgroundColor};
+  border-radius: 8px;
+`;
+
+export const EventItemInnerContents = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  width: ${EVENT_INNER_WIDTH};
+`;
+
+export const InnerText = styled.Text`
+  font-size: 14px;
+  font-weight: 700;
+  color: ${(props) => props.fontColor};
+`;
