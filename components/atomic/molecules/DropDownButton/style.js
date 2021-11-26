2fd03d0e92b6ddb0b7b0d7bf597548e4c0ccab79
yoon-junseo <ysj6844@naver.com>
2021-11-27 03:38:22 +0900

    Feature: DropDownButton
    1. 컴포넌트
    2. 스타일
    #13

---
+++ components/atomic/molecules/DropDownButton/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const Touchable = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  width: 90px;
+`;
