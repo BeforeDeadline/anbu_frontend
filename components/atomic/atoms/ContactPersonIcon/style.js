b32bb810f79d5353b7aa78c31ef4db6106183ae1
yoon-junseo <ysj6844@naver.com>
2021-11-26 22:53:04 +0900

    Style: ContactPersonIcon 스타일 수정
    1. 넓이
    2. 높이
    3. 아이콘 크기
    #11

--- components/atomic/atoms/ContactPersonIcon/style.js
+++ components/atomic/atoms/ContactPersonIcon/style.js
@@ -4,8 +4,8 @@ export const Container = styled.View`
   align-items: center;
   justify-content: center;
 
-  width: 32px;
-  height: 32px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
 
   background-color: ${(props) => props.backgroundColor};
   border-radius: 100px;
