ec47cd1381e9d5a369db3f47cebd5e9fc4eb2e0d
yoon-junseo <ysj6844@naver.com>
2021-11-27 12:59:55 +0900

    Feature: 모달 사용을 위한 수정
    1. 모달에서 색상 변경 되게 하기 위해서 숫자 지정
    #17

--- components/NavFriendListComponent/FriendListContainer/index.jsx
+++ components/NavFriendListComponent/FriendListContainer/index.jsx
@@ -1,61 +1,63 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
 
 import CustomText from "../../atomic/atoms/Text";
 
-import FriendListBox from "../../common/FriendListBox";
 import HomeFriendListBox from "../../common/HomeFriendListBox";
+
+import Modal from "../../atomic/molecules/Modal";
+
+import { color } from "../../../utils/constants";
 
 import { friendList } from "../../../mock/friendsList";
 
 import { Container, InfoDescriptionBox, ScrollViewContainer } from "./style";
 
-const color = [
-  {
-    iconBackgroundColor: "#CEF7E7",
-    iconColor: "#0BD588",
-  },
-  {
-    iconBackgroundColor: "#CEE3F7",
-    iconColor: "#0B74D5",
-  },
-  {
-    iconBackgroundColor: "#FDD8E1",
-    iconColor: "#F33C68",
-  },
-  {
-    iconBackgroundColor: "#F9D4A8",
-    iconColor: "#EF9326",
-  },
-];
-Math.floor(Math.random() * 4);
-const FriendListContainer = () => {
+const FriendListContainer = ({ navigation }) => {
+  const [visible, setVisible] = useState(false);
+  const [randomNum, setRandomNum] = useState(0);
+
+  const toggleBottomNavigationView = () => {
+    setVisible((prev) => !prev);
+  };
+
   return (
-    <Container>
-      <InfoDescriptionBox>
-        <CustomText fontSize="12px" fontWeight="600" fontColor="#FFFFFF">
-          친구142명
-        </CustomText>
-        <CustomText fontSize="12px" fontWeight="400" fontColor="#B5B5B5">
-          연락일 가까운 순
-        </CustomText>
-      </InfoDescriptionBox>
-      <ScrollViewContainer>
-        {friendList.map((friend, idx) => {
-          const randomNumber = Math.floor(Math.random() * 4);
-          return (
-            <HomeFriendListBox
-              key={idx}
-              name={friend.name}
-              contactCycle={friend.contactCycle}
-              tags={friend.tags}
-              iconBackgroundColor={color[randomNumber].iconBackgroundColor}
-              iconColor={color[randomNumber].iconColor}
-            />
-          );
-        })}
-      </ScrollViewContainer>
-    </Container>
+    <>
+      <Container>
+        <InfoDescriptionBox>
+          <CustomText fontSize="12px" fontWeight="600" fontColor="#FFFFFF">
+            친구23명
+          </CustomText>
+          <CustomText fontSize="12px" fontWeight="400" fontColor="#B5B5B5">
+            연락일 가까운 순
+          </CustomText>
+        </InfoDescriptionBox>
+        <ScrollViewContainer>
+          {friendList.map((friend, idx) => {
+            const randomNumber = idx % 4;
+            return (
+              <HomeFriendListBox
+                name={friend.name}
+                contactCycle={friend.contactCycle}
+                tags={friend.tags}
+                iconBackgroundColor={color[randomNumber].iconBackgroundColor}
+                iconColor={color[randomNumber].iconColor}
+                toggleBottomNavigationView={() => {
+                  toggleBottomNavigationView();
+                  setRandomNum(idx % 4);
+                }}
+              />
+            );
+          })}
+        </ScrollViewContainer>
+      </Container>
+      <Modal
+        type="AnbuModal"
+        randomNum={randomNum}
+        visible={visible}
+        toggleBottomNavigationView={toggleBottomNavigationView}
+        navigation={navigation}
+      />
+    </>
   );
 };
 
