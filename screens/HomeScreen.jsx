e855c75cfb31d6fd94f2097ddbfcef1109061a89
yoon-junseo <ysj6844@naver.com>
2021-11-28 07:24:58 +0900

    Feature: HomeScreen
    #21

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,34 +1,20 @@
-// import React from "react";
-
-// import FloatingButton from "../components/atomic/atoms/FloatingButton";
-
-// import NavigationTitle from "../navigaton/NavigationTitle";
-// import Layout from "../components/Layout";
-
-// import GreetingBox from "../components/HomeComponents/GreetingBox";
-// import EventBox from "../components/HomeComponents/EventBox";
-// import AnbuBox from "../components/HomeComponents/AnbuBox";
-
-// const HomeScreen = ({ navigation }) => {
-//   return (
-//     <Layout>
-//       <NavigationTitle title="home" />
-//       <GreetingBox name="윤준서" />
-//       <EventBox />
-//       <AnbuBox type="anbu" />
-//       <AnbuBox type="birthday" />
-//       <FloatingButton navigation={navigation} />
-//     </Layout>
-//   );
-// };
-
-// export default HomeScreen;
-
-import React, { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
 import * as Contacts from "expo-contacts";
 
-export default function HomeScreen() {
+import { storeData } from "../utils/asyncStorage";
+
+import FloatingButton from "../components/atomic/atoms/FloatingButton";
+
+import NavigationTitle from "../navigaton/NavigationTitle";
+import Layout from "../components/Layout";
+
+import GreetingBox from "../components/HomeComponents/GreetingBox";
+import EventBox from "../components/HomeComponents/EventBox";
+import AnbuBox from "../components/HomeComponents/AnbuBox";
+
+const HomeScreen = ({ navigation }) => {
+  const [data, setData] = useState(null);
+
   useEffect(() => {
     (async () => {
       const { status } = await Contacts.requestPermissionsAsync();
@@ -38,23 +24,26 @@ export default function HomeScreen() {
         });
 
         if (data.length > 0) {
-          const contact = data[0];
-          console.log(contact);
-          console.log("data: ", data);
+          setData(data);
         }
       }
     })();
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Contacts Module Example</Text>
-    </View>
-  );
-}
+  useEffect(async () => {
+    await storeData({ key: "contactList", data });
+  }, [data]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+  return (
+    <Layout>
+      <NavigationTitle title="home" />
+      <GreetingBox name="윤준서" />
+      <EventBox />
+      <AnbuBox type="anbu" />
+      <AnbuBox type="birthday" />
+      <FloatingButton navigation={navigation} />
+    </Layout>
+  );
+};
+
+export default HomeScreen;
