a8449d8625e006bebab3807b4f8740376721d215
yoon-junseo <ysj6844@naver.com>
2021-11-28 06:34:04 +0900

    Feature: 연락처 저장하기
    #21

--- components/NavFriendListComponent/FriendListContainer/index.jsx
+++ components/NavFriendListComponent/FriendListContainer/index.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import CustomText from "../../atomic/atoms/Text";
 
@@ -15,45 +17,84 @@ import { Container, InfoDescriptionBox, ScrollViewContainer } from "./style";
 const FriendListContainer = ({ navigation }) => {
   const [visible, setVisible] = useState(false);
   const [randomNum, setRandomNum] = useState(0);
+  const [contactList, setContactList] = useState(null);
+  const [info, setInfo] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const toggleBottomNavigationView = () => {
     setVisible((prev) => !prev);
   };
 
+  const getData = async (key) => {
+    try {
+      const data = await AsyncStorage.getItem(key);
+      if (data) setContactList(data);
+      return JSON.parse(data);
+    } catch (e) {
+      console.log(e);
+      alert("정보를 불러오지 못했습니다. 앱을 다시 시작해주세요!");
+    }
+  };
+
+  useEffect(async () => {
+    const data = await getData("contactList");
+    if (data) {
+      setContactList(data);
+      setLoading(true);
+    }
+  }, []);
+
   return (
     <>
-      <Container>
-        <InfoDescriptionBox>
-          <CustomText fontSize="12px" fontWeight="600" fontColor="#FFFFFF">
-            친구23명
-          </CustomText>
-          <CustomText fontSize="12px" fontWeight="400" fontColor="#B5B5B5">
-            연락일 가까운 순
-          </CustomText>
-        </InfoDescriptionBox>
-        <ScrollViewContainer>
-          {friendList.map((friend, idx) => {
-            const randomNumber = idx % 4;
-            return (
-              <HomeFriendListBox
-                name={friend.name}
-                contactCycle={friend.contactCycle}
-                tags={friend.tags}
-                iconBackgroundColor={color[randomNumber].iconBackgroundColor}
-                iconColor={color[randomNumber].iconColor}
-                toggleBottomNavigationView={() => {
-                  toggleBottomNavigationView();
-                  setRandomNum(idx % 4);
-                }}
-              />
-            );
-          })}
-        </ScrollViewContainer>
-      </Container>
+      {loading ? (
+        <Container>
+          <InfoDescriptionBox>
+            <CustomText fontSize="12px" fontWeight="600" fontColor="#FFFFFF">
+              친구 {contactList.length}명
+            </CustomText>
+            <CustomText fontSize="12px" fontWeight="400" fontColor="#B5B5B5">
+              연락일 가까운 순
+            </CustomText>
+          </InfoDescriptionBox>
+          <ScrollViewContainer>
+            {contactList.map((contact, idx) => {
+              const randomNumber = idx % 4;
+              return (
+                <HomeFriendListBox
+                  key={idx}
+                  name={contact.name}
+                  contactCycle={friendList[idx % 12].contactCycle}
+                  tags={friendList[idx % 12].tags}
+                  iconBackgroundColor={color[randomNumber].iconBackgroundColor}
+                  iconColor={color[randomNumber].iconColor}
+                  toggleBottomNavigationView={() => {
+                    toggleBottomNavigationView();
+                    setRandomNum(idx % 4);
+                    setInfo({
+                      name: contact.name,
+                      phoneNumber:
+                        contact.phoneNumbers && contact.phoneNumbers[0].number,
+                      tags: friendList[idx % 12].tags,
+                      contactCycle: friendList[idx % 12].contactCycle,
+                    });
+                  }}
+                />
+              );
+            })}
+          </ScrollViewContainer>
+        </Container>
+      ) : (
+        <View
+          style={{ flex: 1, justifyContents: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" color="#0BD588" />
+        </View>
+      )}
       <Modal
         type="AnbuModal"
         randomNum={randomNum}
         visible={visible}
+        info={info}
         toggleBottomNavigationView={toggleBottomNavigationView}
         navigation={navigation}
       />
