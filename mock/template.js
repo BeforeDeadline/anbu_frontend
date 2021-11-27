c0cfefabb3d37173cb1c20c2c86e4d23f9acdff4
yoon-junseo <ysj6844@naver.com>
2021-11-27 22:18:27 +0900

    Fix: MockData
    #16

--- mock/template.js
+++ mock/template.js
@@ -1,22 +1,67 @@
 export const greetingList = [
-  ["주말동안 행복한 시간 되셨나요?", "잘지내셨어요?"],
-  ["하이", "잘지내셨어요?", "gggggg"],
-  ["하이", "잘지내셨어요?", "gggggg"],
-  ["하이", "잘지내셨어요?", "gggggg"],
+  [
+    { id: 1, template: "주말동안 행복한 시간 되셨나요?" },
+    { id: 2, template: "잘지내셨어요?" },
+  ],
+  [
+    { id: 3, template: "하이" },
+    { id: 4, template: "잘지내셨어요?" },
+    { id: 5, template: "gggggg" },
+  ],
+  [
+    { id: 6, template: "하이" },
+    { id: 7, template: "잘지내셨어요?" },
+    { id: 8, template: "gggggg" },
+  ],
+  [
+    { id: 9, template: "하이" },
+    { id: 10, template: "잘지내셨어요?" },
+    { id: 11, template: "gggggg" },
+  ],
 ];
 
 export const bodyList = [
-  ["풍성한 한가위 보내세요", "zzzzzzz"],
-  ["ggggggggggggg", "4444ggg"],
-  ["ggggggggggggg", "4444ggg"],
-  ["ggggggggggggg", "4444ggg"],
+  [
+    { id: 12, template: "주말동안 행복한 시간 되셨나요?" },
+    { id: 13, template: "잘지내셨어요?" },
+  ],
+  [
+    { id: 14, template: "하이" },
+    { id: 15, template: "잘지내셨어요?" },
+    { id: 16, template: "gggggg" },
+  ],
+  [
+    { id: 17, template: "하이" },
+    { id: 18, template: "잘지내셨어요?" },
+    { id: 19, template: "gggggg" },
+  ],
+  [
+    { id: 20, template: "하이" },
+    { id: 21, template: "잘지내셨어요?" },
+    { id: 22, template: "gggggg" },
+  ],
 ];
 
 export const finishList = [
-  ["풍성한 한가위 보내세요", "zzzzzzz"],
-  ["풍성한 한가위 보내세요", "zzzzzzz"],
-  ["풍성한 한가위 보내세요", "zzzzzzz"],
-  ["ggggggggggggg", "4444ggg"],
+  [
+    { id: 23, template: "주말동안 행복한 시간 되셨나요?" },
+    { id: 24, template: "잘지내셨어요?" },
+  ],
+  [
+    { id: 25, template: "하이" },
+    { id: 26, template: "잘지내셨어요?" },
+    { id: 27, template: "gggggg" },
+  ],
+  [
+    { id: 28, template: "하이" },
+    { id: 29, template: "잘지내셨어요?" },
+    { id: 30, template: "gggggg" },
+  ],
+  [
+    { id: 31, template: "하이" },
+    { id: 32, template: "잘지내셨어요?" },
+    { id: 33, template: "gggggg" },
+  ],
 ];
 
 // export const greetingList = [
