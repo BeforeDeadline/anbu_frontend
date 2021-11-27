335a423ca2c178d6740f2572ec7210b39ce0dd3d
yoon-junseo <ysj6844@naver.com>
2021-11-28 07:21:30 +0900

    Fix: console.log 제거
    #21

--- utils/asyncStorage.js
+++ utils/asyncStorage.js
@@ -3,7 +3,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export const storeData = async ({ key, data }) => {
   try {
     await AsyncStorage.setItem(key, JSON.stringify(data));
-    console.log("저장 완료");
   } catch (e) {
     alert("앱을 다시 시작해주세요");
   }
